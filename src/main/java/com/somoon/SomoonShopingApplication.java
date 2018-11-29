a0cc11570b85553ef08ecc910887de302f7b61ba
SISparang <goflvhxj2547@gmail.com>
2018-11-29 14:47:20 +0900

    소문 - 쇼핑몰 프로젝트, 1

---
+++ src/main/java/com/somoon/SomoonShopingApplication.java
@@ -0,0 +1,12 @@
+package com.somoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomoonShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomoonShopingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/somoon/SomoonShopingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.somoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SomoonShopingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
